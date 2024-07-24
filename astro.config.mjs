a5c6052c81971fd8d5f484cbb5325d15a108f464
Himel Majumder <himelmajumder1@gmail.com>
2024-07-24 17:55:39 +0200

    feat: Update Astro configuration, add @astrojs/vercel package, and enable web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
-// import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
-
+// https://astro.build/config
 export default defineConfig({
   site: 'https://himelmaj.vercel.app/',
   prefetch: true,
-	devToolbar: {
-		enabled: false,
-	},
+  devToolbar: {
+    enabled: false
+  },
   integrations: [tailwind(), astroI18next(), sitemap()],
-  // adapter: vercel({
-  //   webAnalytics: { enabled: true }
-  // }),
-  // output: "server",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
