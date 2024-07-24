2f5631fa805710f2f2a4374a112df91659c8a121
Himel Majumder <himelmajumder1@gmail.com>
2024-07-24 18:01:50 +0200

    chore: Update Astro configuration and remove @astrojs/vercel/serverless package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://himelmaj.vercel.app/',
   prefetch: true,
-  devToolbar: {
-    enabled: false
-  },
+	devToolbar: {
+		enabled: false,
+	},
   integrations: [tailwind(), astroI18next(), sitemap()],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  })
 });
