a8c03e5f427034ecb6da334079bac5bb825a9218
Himel Majumder <himelmajumder1@gmail.com>
2024-07-24 17:24:02 +0200

    feat: Remove partytown integration and update devToolbar configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
-import partytown from '@astrojs/partytown';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://himelmaj.vercel.app/',
-  integrations: [tailwind(), astroI18next(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), sitemap()]
+  prefetch: true,
+	devToolbar: {
+		enabled: false,
+	},
+  integrations: [tailwind(), astroI18next(), sitemap()]
 });
