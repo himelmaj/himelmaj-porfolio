3228c52da290ab592fb5c6fbeacffe9dbfbcae3e
Himel Majumder <himelmajumder1@gmail.com>
2024-07-24 17:37:39 +0200

    feat: Add Vercel adapter for serverless deployment and enable web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
-
+import vercel from "@astrojs/vercel/serverless"
 // https://astro.build/config
 export default defineConfig({
   site: 'https://himelmaj.vercel.app/',
@@ -10,5 +10,9 @@ export default defineConfig({
 	devToolbar: {
 		enabled: false,
 	},
-  integrations: [tailwind(), astroI18next(), sitemap()]
+  integrations: [tailwind(), astroI18next(), sitemap()],
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
+  output: "server",
 });
