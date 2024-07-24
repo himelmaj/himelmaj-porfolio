d7f9f6da29baf2f7d6d42bdc5f41c848a3e4cee9
Himel Majumder <himelmajumder1@gmail.com>
2024-07-24 17:19:05 +0200

    feat: Add lazy loading for images, update npm dependencies, and integrate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
+import partytown from '@astrojs/partytown';
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
+  site: 'https://himelmaj.vercel.app/',
+  integrations: [tailwind(), astroI18next(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()]
 });
