5c54b8c89836edfbd87165e136d3064c24e18c45
Himel Majumder <himelmajumder1@gmail.com>
2024-07-25 22:13:28 +0200

    chore: Update astro.config.mjs and package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://himelmaj.vercel.app/',
   prefetch: true,
-	devToolbar: {
-		enabled: false,
-	},
-  integrations: [tailwind(), astroI18next(), sitemap()],
+  devToolbar: {
+    enabled: false
+  },
+  integrations: [tailwind(), astroI18next(), sitemap(), partytown({
+    config: {
+      forward: ['dataLayer:push']
+    }
+  })]
 });
